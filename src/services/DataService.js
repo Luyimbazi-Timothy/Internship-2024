28a2eb80531b1c8b8b8efca4776f937a234e0d59
roy nsumba <83820529+roynsumba@users.noreply.github.com>
2024-07-17 05:59:37 +0300

    feat: data service simulating async data fetch with dummy data.

---
+++ src/services/DataService.js
@@ -0,0 +1,37 @@
+// src/services/dataService.js
+import cow1 from '../photos/cow1.jpg';
+import cow2 from '../photos/cow2.jpg';
+import goat1 from '../photos/goat1.jpg';
+import goat2 from '../photos/goat2.jpg';
+
+const fetchData = async () => {
+  // Simulate fetching data from a database or an API
+  const tableData = [
+    {
+      period: '2024 Q1',
+      perspective: 'Financial',
+      ssMartaObjectives: 'Increase Revenue',
+      initiative: 'Expand Market',
+      measurableActivities: 'Increase sales by 10%',
+      implementations: 'Implemented new sales strategies',
+      comments: 'Quarterly goals met',
+      stakeholders: 'Sales team, Marketing',
+      evidence: [cow1, goat2],
+    },
+    {
+      period: '2024 Q2',
+      perspective: 'Customer',
+      ssMartaObjectives: 'Enhance Customer Satisfaction',
+      initiative: 'Improve Service Quality',
+      measurableActivities: 'Reduce complaints by 20%',
+      implementations: 'Conducted customer feedback surveys',
+      comments: 'Positive feedback received',
+      stakeholders: 'Customer support, Quality assurance',
+      evidence: [goat1, cow2],
+    },
+    
+  ];
+  return tableData;
+};
+
+export default fetchData;
