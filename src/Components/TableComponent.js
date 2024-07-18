606c13695d43dd6a82319e14d53ba94dc48ef249
roy nsumba <83820529+roynsumba@users.noreply.github.com>
2024-07-18 10:36:25 +0300

    feat: switch to image name instead of image display

--- src/Components/TableComponent.js
+++ src/Components/TableComponent.js
@@ -17,24 +17,29 @@ const TableComponent = ({ data, onRowClick }) => {
           <th>EVIDENCE</th>
         </tr>
       </thead>
-      <tbody>
-        {data.map((row, index) => (
-          <tr key={index} onClick={() => onRowClick(index)} style={{ cursor: 'pointer' }}>
-            <td>{row.period}</td>
-            <td>{row.perspective}</td>
-            <td>{row.ssMartaObjectives}</td>
-            <td>{row.initiative}</td>
-            <td>{row.measurableActivities}</td>
-            <td>{row.implementations}</td>
-            <td>{row.comments}</td>
-            <td>{row.stakeholders}</td>
-            <td>
-              {row.evidence.map((img, i) => (
-                <img key={i} src={img} alt={`Evidence ${i + 1}`} style={{ width: '50px', height: '50px', marginRight: '5px' }} />
-              ))}
-            </td>
-          </tr>
-        ))}
+      <tbody> 
+        {data.map((row, index) => { 
+          console.log("DATA", data)
+          console.log(`Row ${index + 1} evidence: `, row.evidence); // Debugging line
+          return (
+            <tr key={index} onClick={() => onRowClick(index)} style={{ cursor: 'pointer' }}>
+              <td>{row.period}</td>
+              <td>{row.perspective}</td>
+              <td>{row.ssMartaObjectives}</td>
+              <td>{row.initiative}</td>
+              <td>{row.measurableActivities}</td>
+              <td>{row.implementations}</td>
+              <td>{row.comments}</td>
+              <td>{row.stakeholders}</td>
+              <td>
+                {row.evidence.map((img, i) => {
+                  const imgName = img.split('/').pop().split('.')[0]; // Extract the file name without extension
+                  return <span key={i}>{imgName} </span>;
+                })}
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
