dae187fee4a748d243ab12ce96544830b9bbb3ad
Luyimbazi-Timothy <eclastimothy@gmail.com>
2024-07-19 10:57:51 +0300

    feat: Adding css and upload functionality to the form

--- src/Pages/AppraisalActivity/schema.js
+++ src/Pages/AppraisalActivity/schema.js
@@ -1,16 +1,16 @@
 import * as Yup from 'yup'
 
 const validationSchema = Yup.object({
-    period: Yup.string().required('Required'),
-    perspective: Yup.string().required('Required'),
-    ssMartaObjectives: Yup.string().required('Required'),
-    initiative: Yup.string().required('Required'),
-    measurableActivities: Yup.string().required('Required'),
-    implementations: Yup.string().required('Required'),
-    date: Yup.string().required('Required*'),
+    period: Yup.string().required('Required*'),
+    perspective: Yup.string().required('Required*'),
+    ssMartaObjectives: Yup.string().required('Required*'),
+    initiative: Yup.string().required('Required*'),
+    measurableActivities: Yup.string().required('Required*'),
+    implementations: Yup.string().required('Required*'),
+    date: Yup.date().required('Required*'),
     comments: Yup.string(),
     stakeholders: Yup.string(),
-    evidence: Yup.string(),
+    evidence: Yup.mixed().required('Required*')
   })
 
 export default validationSchema  
