ba47a74e86bf20b7ae6cf841e9c587160b701334
Nakatolynn <54796485+Nakatolynn@users.noreply.github.com>
2024-07-19 16:54:29 +0300

    Feat:Created a Config File
    Created a Config File

---
+++ src/CommonConfigs/Configs.js
@@ -0,0 +1,13 @@
+const quarterDropdownOptions = [
+    { key: 'Select a quarter', value: '' },
+    { key: 'Q1', value: 'quarter1' },
+    { key: 'Q2', value: 'quarter2' },
+    { key: 'Q3', value: 'quarter3' },
+    { key: 'Q4', value: 'quarter4' }
+  ]
+
+const Configs={
+    quarterDropdownOptions  
+};
+
+export default Configs;
