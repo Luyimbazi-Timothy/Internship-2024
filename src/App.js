dbb900a73124962ab4430d5646dbd26f2b80da8e
Luyimbazi-Timothy <eclastimothy@gmail.com>
2024-07-27 17:26:21 +0300

    feat: Adding ControlPanel UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Layout from './layout/fullPage/Layout';
 import { DisplaySidebarProvider } from './Components/ContextHook/useDisplayContext';
 
-
 function App() {
 
   return (
