bc168bce68fe234c4908405dd5ddf26a4e167601
Marios Turyasingura <mturyasingura@ura.go.ug>
2024-07-30 15:48:15 +0300

    fix: pull errors

--- src/Router/Routing.jsx
+++ src/Router/Routing.jsx
@@ -1,11 +1,11 @@
 // src/Router/Routing.js
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard/Dashboard";
+import Dashboard from "../pages/dashboard/Dashboard";
 import ControlPanel from "../pages/ControlPanel";
-import PageNotFound from "../pages/Error/PageNotFound";
-import DetailPage from "../pages/AppraisalActivity/ActivityTableData/DetailPage";
-import Login from "../pages/Auth/Login";
+import PageNotFound from "../pages/error/PageNotFound";
+import DetailPage from "../pages/AppraisalActivity/activityTableData/DetailPage";
+import Login from "../pages/auth/Login";
 
 function Routing() {
   return (
