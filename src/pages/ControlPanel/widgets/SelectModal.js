c2df4e156bb9912141cd93fe3341fb37ddfbabd7
Marios Turyasingura <mturyasingura@ura.go.ug>
2024-07-31 09:31:47 +0300

    feat: modifications made to handleChange function

--- src/pages/ControlPanel/widgets/SelectModal.js
+++ src/pages/ControlPanel/widgets/SelectModal.js
@@ -2,49 +2,36 @@ import {  React, useContext} from 'react'
 import TextField from '@mui/material/TextField'
 import Autocomplete from '@mui/material/Autocomplete'
 import { Context } from '../ControlPanel'
+import { dataSets, controlPanelOptions } from './ControlPanelConfig'
 
 
-function SelectModal() {
+function SelectModal({setAddBtnLabel}) {
 
-    const [setFieldId, setColumnHeader, setTableData ] = useContext(Context)
-
-    const dataSets = {
-        1: [{ field: 'Quarter 1' }, { field: 'Quarter 2' }, { field: 'Quarter 3' }, { field: 'Quarter 4' }],
-        2: [{ field: 'Client' }, { field: 'Financial' }],
-        3: [{ field: 'Increase Revenue' }, { field: 'Increase Brand Awareness' }],
-        4: [{ field: 'Develop tax collection applications' }, { field: 'Create a new marketing plan' }],
-        5: [{ field: 'Develop E-Ttax' }, { field: 'Develop a New Marketing Strategy' }],
-      }
-
-
-    const controlPanelOptions = [
-        { label: 'Period', id: 1 },
-        { label: 'Perspective', id: 2 },
-        { label: 'Ssmarta Objectives', id: 3 },
-        { label: 'Initiatives', id: 4 },
-        { label: 'Activities', id: 5 },
-      ]
+    const {setColumnHeader, setTableData, setShowTableData } = useContext(Context)
 
       const handleChange = (event, value) => {
         if (value) {
-          setFieldId(value.id)
-          setTableData(dataSets[value.id] || [])
-          setColumnHeader(value.label)
+          setTableData(dataSets[value.id] || []);
+          setColumnHeader(value.label);
+          setAddBtnLabel(value.label);
+          setShowTableData(true);
         } else {
-          setFieldId('')
-          setTableData([])
-          setColumnHeader('Field')
+          setTableData([]);
+          setColumnHeader('Field');
+          setShowTableData(false);
+          setAddBtnLabel('');
         }
-      }  
+      };
 
     return (
     <>
+    <br/>
     <Autocomplete
               disablePortal
               id="controlPanelOptions"
               options={controlPanelOptions}
-              sx={{ width: 130 }}
-              renderInput={(params) => <TextField  {...params} label="Select" />}
+              sx={{ width: 600 }}
+              renderInput={(params) => <TextField  {...params} label="Select an Input field to configure" />}
               clearOnEscape
               onChange={handleChange}
               isOptionEqualToValue={(option, value) => option.id === value.id}
