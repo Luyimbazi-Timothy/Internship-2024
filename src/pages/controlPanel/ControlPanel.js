b8bec7111d09ea9e0f512cfbcb05e58c89640222
Luyimbazi-Timothy <eclastimothy@gmail.com>
2024-08-09 18:38:17 +0300

    chore: troubleshooting

--- src/pages/controlPanel/ControlPanel.js
+++ src/pages/controlPanel/ControlPanel.js
@@ -1,121 +1,173 @@
-import { useState, React, createContext, useEffect } from 'react'
-import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
-import TableData from './widgets/TableData'
-import DeleteDialogBox from './widgets/DeleteDialogBox'
-import SelectModal from './widgets/SelectModal'
-import AddModal from './widgets/AddModal'
-import axios from 'axios'
-import urlConfig from '../../services/Urls'
+import { useState, React, createContext, useEffect } from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import TableData from "./widgets/TableData";
+import DeleteDialogBox from "./widgets/DeleteDialogBox";
+import SelectModal from "./widgets/SelectModal";
+import AddModal from "./widgets/AddModal";
+import axios from "axios";
+import urlConfig from "../../services/Urls";
 
-export const Context = createContext()
+export const Context = createContext();
 
 const ControlPanel = () => {
-
-
-  const [columnHeader, setColumnHeader] = useState('Field')
-  const [isPreview, setIsPreview] = useState(false)
-  const [formOpen, setFormOpen] = useState(false)
-  const [editData, setEditData] = useState(null)
-  const [alertOpen, setAlertOpen] = useState(false)
-  const [rowToDelete, setRowToDelete] = useState(null)
-  const [tableData, setTableData] = useState([])
-  const [showTableData, setShowTableData] = useState(false)
-  const [addBtnLabel, setAddBtnLabel] = useState("")
-
+  const [columnHeader, setColumnHeader] = useState("Field");
+  const [isPreview, setIsPreview] = useState(false);
+  const [formOpen, setFormOpen] = useState(false);
+  const [editData, setEditData] = useState(null);
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [rowToDelete, setRowToDelete] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const [showTableData, setShowTableData] = useState(false);
+  const [addBtnLabel, setAddBtnLabel] = useState("");
+  const [noOfNewUpdates, setNoOfNewUpdates] = useState(0);
 
   const [dataSets, setDataSets] = useState({
     1: [],
     2: [],
     3: [],
     4: [],
-    5: []
+    5: [],
   });
 
-  
-
-  useEffect(() => {
-
-    const fetchData = async () => {
+  const fetchData = async () => {
     const periods = await axios.get(`${urlConfig.allPeriodsUrl}`);
     const perspectives = await axios.get(`${urlConfig.allPerspectivesUrl}`);
     const objectives = await axios.get(`${urlConfig.allSsmartaObjectiveUrl}`);
     const initiatives = await axios.get(`${urlConfig.allInitiativesUrl}`);
-    const activities = await axios.get(`${urlConfig.allMeasurableActivitiesUrl}`);
+    const activities = await axios.get(
+      `${urlConfig.allMeasurableActivitiesUrl}`
+    );
 
-
-    
-    axios.all([periods, perspectives, objectives, initiatives, activities]).then(axios.spread((periods, perspectives, objectives, initiatives, activities) => {
-        const updatedDataSets = { ...dataSets };
-              periods.data.forEach(item => {
-              updatedDataSets[1].push({ field: item.fieldDescription , id: item.itemId});
+    axios
+      .all([periods, perspectives, objectives, initiatives, activities])
+      .then(
+        axios.spread(
+          (periods, perspectives, objectives, initiatives, activities) => {
+            const updatedDataSets = { ...dataSets };
+            periods.data.forEach((item) => {
+              updatedDataSets[1].push({ //all periods
+                field: item.fieldDescription,
+                id: item.itemId,
               });
-              perspectives.data.forEach(item => {
-              updatedDataSets[2].push({ field: item.fieldDescription , id: item.itemId});
+            });
+            perspectives.data.forEach((item) => {
+              updatedDataSets[2].push({
+                field: item.fieldDescription,
+                id: item.itemId,
               });
-              objectives.data.forEach(item => {
-              updatedDataSets[3].push({ field: item.fieldDescription , id: item.itemId});
-                });
-              initiatives.data.forEach(item => {
-              updatedDataSets[4].push({ field: item.fieldDescription , id: item.itemId});
+            });
+            objectives.data.forEach((item) => {
+              updatedDataSets[3].push({
+                field: item.fieldDescription,
+                id: item.itemId,
               });
-              activities.data.forEach(item => {
-              updatedDataSets[5].push({ field: item.fieldDescription , id: item.itemId});
+            });
+            initiatives.data.forEach((item) => {
+              updatedDataSets[4].push({
+                field: item.fieldDescription,
+                id: item.itemId,
               });
+            });
+            activities.data.forEach((item) => {
+              updatedDataSets[5].push({
+                field: item.fieldDescription,
+                id: item.itemId,
+              });
+            });
 
-              setDataSets(updatedDataSets);
+            setDataSets(updatedDataSets);
+            console.log("periods from dataSet", dataSets[1]);
 
-
-    })).catch(errors => {
+          }
+        )
+      )
+      .catch((errors) => {
         console.error(errors);
-    });
-  }
-  fetchData()
+      });
+  };
+
+  useEffect(() => {
     
-
-  },[] ); 
-
+    fetchData();
+  }, [noOfNewUpdates]);
 
   const handleAdd = () => {
-    setFormOpen(true)
-    setIsPreview(false)
-    setEditData(null)
-  }
-
-
+    setFormOpen(true);
+    setIsPreview(false);
+    setEditData(null);
+  };
 
   return (
     <>
       <Container minWidth="sm">
         <Grid container spacing={1}>
-
           {/* Select Field to configure  */}
           <Grid item xs={12}>
-            <Context.Provider value={{setShowTableData, setColumnHeader, setTableData, dataSets}}>
-              <SelectModal setAddBtnLabel={setAddBtnLabel}/>
+            <Context.Provider
+              value={{
+                setShowTableData,
+                setColumnHeader,
+                setTableData,
+                dataSets,
+              }}
+            >
+              <SelectModal setAddBtnLabel={setAddBtnLabel} />
             </Context.Provider>
           </Grid>
           {/* Add / Edit Modal  */}
-          <Context.Provider value={{isPreview, formOpen, setFormOpen, tableData, setTableData, editData, setEditData, addBtnLabel}}>
-            <AddModal />
+          <Context.Provider
+            value={{
+              formOpen,
+              setFormOpen,
+              tableData,
+              setTableData,
+              editData,
+              setEditData,
+              addBtnLabel,
+              setNoOfNewUpdates,
+              dataSets
+            }}
+          >
+            <AddModal isPreview={isPreview}/>
           </Context.Provider>
           {/* Delete dialog box  */}
-          <Context.Provider value={{rowToDelete, tableData, alertOpen, setTableData, setAlertOpen, editData, addBtnLabel}}>
+          <Context.Provider
+            value={{
+              rowToDelete,
+              tableData,
+              alertOpen,
+              setTableData,
+              setAlertOpen,
+              editData,
+              addBtnLabel,
+            }}
+          >
             <DeleteDialogBox />
           </Context.Provider>
-          {showTableData &&
+          
+          {showTableData && (
             <Grid item xs={12}>
               {/* table data  */}
-              <Context.Provider value={{columnHeader, tableData, setFormOpen, setEditData, setIsPreview, setRowToDelete, setAlertOpen}}>
-                <TableData handleAdd={handleAdd} addBtnLabel={addBtnLabel}/>
+              <Context.Provider
+                value={{
+                  columnHeader,
+                  tableData,
+                  setFormOpen,
+                  setEditData,
+                  setIsPreview,
+                  setRowToDelete,
+                  setAlertOpen,
+                }}
+              >
+                <TableData handleAdd={handleAdd} addBtnLabel={addBtnLabel} />
               </Context.Provider>
             </Grid>
-          }
+          )}
         </Grid>
       </Container>
     </>
-  )
+  );
+};
 
-}
-
-export default ControlPanel
+export default ControlPanel;
--- src/pages/controlPanel/widgets/AddModal.js
+++ src/pages/controlPanel/widgets/AddModal.js
@@ -1,71 +1,94 @@
-import {  React, useContext} from 'react'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import { Formik, Form, Field, ErrorMessage } from "formik"
-import Dialog from '@mui/material/Dialog'
-import DialogContent from '@mui/material/DialogContent'
-import DialogTitle from '@mui/material/DialogTitle'
-import Grid from '@mui/material/Grid'
-import { Context } from '../ControlPanel'
-import { Box } from '@mui/material'
-import Swal from 'sweetalert2'
-import { controlPanelValidationSchema } from '../../appraisalActivity/addActivity/Schema'
-import axios from 'axios'
-import urlConfig from '../../../services/Urls'
+import { React, useContext } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Grid from "@mui/material/Grid";
+import { Context } from "../ControlPanel";
+import { Box } from "@mui/material";
+import Swal from "sweetalert2";
+import { controlPanelValidationSchema } from "../../appraisalActivity/addActivity/Schema";
+import axios from "axios";
+import urlConfig from "../../../services/Urls";
 
+function AddModal({isPreview}) {
+  const {
+    formOpen,
+    setFormOpen,
+    tableData,
+   setTableData,
+    editData,
+    setEditData,
+    addBtnLabel,
+    setNoOfNewUpdates,
+  } = useContext(Context);
 
-function AddModal() {
-
-  const {isPreview, formOpen, setFormOpen, tableData, setTableData, editData, setEditData, addBtnLabel} = useContext(Context)
-
-  const initialValues = { content: editData ? editData.field : '' }
+  const initialValues = { content: editData ? editData.field : "" };
 
   const handleClose = () => {
-    setFormOpen(false)
-    setEditData(null)
-  }
+    setFormOpen(false);
+    setEditData(null);
+  };
 
   const handleFormSubmit = (values) => {
     if (isPreview) {
-      setTableData(tableData.map((item) =>
-        item === editData ? { field: values.content } : item
-      ))
+      setTableData(
+        tableData.map((item) =>
+          item === editData ? { field: values.content } : item
+        )
+      );
 
-      if(addBtnLabel === 'Perspectives'){
-        axios.put(`http://localhost:5003/api/Perspective/update-a-perspective/${editData?.id}`, {
-          fieldName: 'Perspective',
-          fieldDescription: values.content,
-          userId: urlConfig.loggedInId
-        })
-      }
-      else if(addBtnLabel === 'Initiatives') {
-        axios.put(`http://localhost:5003/api/Initiative/update-an-initiative/${editData?.id}`, {
-          fieldName: 'Initiative',
-          fieldDescription: values.content,
-          userId: urlConfig.loggedInId
-        })
-      }
-      else if(addBtnLabel === 'Periods') {
-        axios.post(`http://localhost:5003/api/Period/update-period-item/${editData?.id}`, {
-          fieldName: 'Period',
-          fieldDescription: values.content,
-          userId: urlConfig.loggedInId
-        })
-      }
-  
-      else if(addBtnLabel === 'Ssmarta Objectives') {
-        axios.post(`http://localhost:5003/api/ssmarta-objective/update-objective-item/${editData?.id}`, {
-          fieldName: 'Objective',
-          fieldDescription: values.content,
-          userId: urlConfig.loggedInId
-        })
-      }
-      else if(addBtnLabel === 'Activities') {
-        axios.post(`http://localhost:5003/api/Activity/update-activity-item/${editData?.id}`, {
-          fieldName: 'Measurable Activity',
-          fieldDescription: values.content,
-          userId: urlConfig.loggedInId
-        })
+      if (addBtnLabel === "Perspectives") {
+        axios.put(
+          `http://localhost:5003/api/Perspective/update-a-perspective/${editData?.id}`,
+          {
+            fieldName: "Perspective",
+            fieldDescription: values.content,
+            userId: urlConfig.loggedInId,
+          }
+        );
+      } else if (addBtnLabel === "Initiatives") {
+        axios.put(
+          `http://localhost:5003/api/Initiative/update-an-initiative/${editData?.id}`,
+          {
+            fieldName: "Initiative",
+            fieldDescription: values.content,
+            userId: urlConfig.loggedInId,
+          }
+        );
+      } else if (addBtnLabel === "Periods") {
+        axios
+          .post(
+            `http://localhost:5003/api/Period/update-period-item/${editData?.id}`,
+            {
+              fieldName: "Period",
+              fieldDescription: values.content,
+              userId: urlConfig.loggedInId,
+            }
+          )
+          .then((response) => {
+            console.log("Added data", response);
+          });
+      } else if (addBtnLabel === "Ssmarta Objectives") {
+        axios.post(
+          `http://localhost:5003/api/ssmarta-objective/update-objective-item/${editData?.id}`,
+          {
+            fieldName: "Objective",
+            fieldDescription: values.content,
+            userId: urlConfig.loggedInId,
+          }
+        );
+      } else if (addBtnLabel === "Activities") {
+        axios.post(
+          `http://localhost:5003/api/Activity/update-activity-item/${editData?.id}`,
+          {
+            fieldName: "Measurable Activity",
+            fieldDescription: values.content,
+            userId: urlConfig.loggedInId,
+          }
+        );
       }
 
       Swal.fire({
@@ -73,117 +96,119 @@ function AddModal() {
         icon: "success",
         title: "Record has been updated succesfully",
         showConfirmButton: false,
-        timer: 1500
-      })
-
+        timer: 1500,
+      });
     } else {
-      const newEntry = { field: values.content }
-      setTableData([...tableData, newEntry])
+      // const newEntry = { field: values.content }
+      // setTableData([...tableData, newEntry])
 
-      if(addBtnLabel === 'Perspectives'){
-        axios.post('http://localhost:5003/api/Perspective/add-a-perspective', {
-          fieldName: 'Perspective',
+      if (addBtnLabel === "Perspectives") {
+        axios.post("http://localhost:5003/api/Perspective/add-a-perspective", {
+          fieldName: "Perspective",
           fieldDescription: values.content,
-          userId: urlConfig.loggedInId
-        })
-        }
-        else if(addBtnLabel === 'Initiatives') {
-          axios.post('http://localhost:5003/api/Initiative/add-an-initiative', {
-            fieldName: 'Initiative',
+          userId: urlConfig.loggedInId,
+        });
+      } else if (addBtnLabel === "Initiatives") {
+        axios.post("http://localhost:5003/api/Initiative/add-an-initiative", {
+          fieldName: "Initiative",
+          fieldDescription: values.content,
+          userId: urlConfig.loggedInId,
+        });
+      } else if (addBtnLabel === "Periods") {
+        axios.post("http://localhost:5003/api/Period/create-period-item", {
+          fieldName: "Period",
+          fieldDescription: values.content,
+          userId: urlConfig.loggedInId,
+        });
+        //update table data to
+        
+        
+      } else if (addBtnLabel === "Ssmarta Objectives") {
+        axios.post(
+          "http://localhost:5003/api/ssmarta-objective/create-objective-item",
+          {
+            fieldName: "Objective",
             fieldDescription: values.content,
-            userId: urlConfig.loggedInId
-          })
-        }
-        else if(addBtnLabel === 'Periods') {
-          axios.post('http://localhost:5003/api/Period/create-period-item', {
-            fieldName: 'Period',
-            fieldDescription: values.content,
-            userId: urlConfig.loggedInId
-          })
-        }
-        else if(addBtnLabel === 'Ssmarta Objectives') {
-          axios.post('http://localhost:5003/api/ssmarta-objective/create-objective-item', {
-            fieldName: 'Objective',
-            fieldDescription: values.content,
-            userId: urlConfig.loggedInId
-          })
-        }
-        else if(addBtnLabel === 'Activities') {
-          axios.post('http://localhost:5003/api/Activity/create-activity-item', {
-            fieldName: 'Measurable Activity',
-            fieldDescription: values.content,
-            userId: urlConfig.loggedInId
-          })
-        }
+            userId: urlConfig.loggedInId,
+          }
+        );
+      } else if (addBtnLabel === "Activities") {
+        axios.post("http://localhost:5003/api/Activity/create-activity-item", {
+          fieldName: "Measurable Activity",
+          fieldDescription: values.content,
+          userId: urlConfig.loggedInId,
+        });
+      }
 
       Swal.fire({
         position: "center",
         icon: "success",
         title: "Item has been added successfully",
         showConfirmButton: false,
-        timer: 1500
-      })
+        timer: 1500,
+      });
 
+      
     }
-    handleClose()
-  }
+    setNoOfNewUpdates((_prev) => _prev + 1);
+    handleClose();
+  };
   return (
     <>
-    <Dialog open={formOpen} onClose={handleClose}>
-            <DialogTitle>{isPreview ? 'Edit details' : 'Add an Item'}</DialogTitle>
-            <DialogContent>
-              <Formik
-                initialValues={initialValues}
-                validationSchema={controlPanelValidationSchema}
-                onSubmit={handleFormSubmit}
-                validateOnChange={true}
-                enableReinitialize
-              >
-                {formik => (
-                  <Form>
-                    <Grid container spacing={2}>
-                      <Grid item xs={12}>
-                        <Field
-                          as={TextField}
-                          label=""
-                          id="content"
-                          name="content"
-                        />
-                        <ErrorMessage
-                          component="div"
-                          name="content"
-                          className="error"
-                        />
-                      </Grid>
-                      <Grid item xs={12}>
-                  <Box display="flex" justifyContent="flex-end">
-                  <Button
-                      type="submit"
-                      disabled={!formik.isValid || formik.isSubmitting}
-                      color="primary"
-                      variant='contained'
-                      sx={{ mr: 1 }}
-                    >
-                      {isPreview ? 'Save' : 'Add'}
-                    </Button>
-                    <Button
-                      onClick={handleClose}
-                      color="error"
-                      variant='contained'
-                    >
-                      Cancel
-                    </Button>
-                    
-                  </Box>
+      <Dialog open={formOpen} onClose={handleClose}>
+        <DialogTitle>{isPreview ? "Edit details" : "Add an Item"}</DialogTitle>
+        <DialogContent>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={controlPanelValidationSchema}
+            onSubmit={handleFormSubmit}
+            validateOnChange={true}
+            enableReinitialize
+          >
+            {(formik) => (
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Field
+                      as={TextField}
+                      label=""
+                      id="content"
+                      name="content"
+                    />
+                    <ErrorMessage
+                      component="div"
+                      name="content"
+                      className="error"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box display="flex" justifyContent="flex-end">
+                      <Button
+                        type="submit"
+                        disabled={!formik.isValid || formik.isSubmitting}
+                        color="primary"
+                        variant="contained"
+                        sx={{ mr: 1 }}
+                      >
+                        {isPreview ? "Save" : "Add"}
+                      </Button>
+                      <Button
+                        onClick={handleClose}
+                        color="error"
+                        variant="contained"
+                      >
+                        Cancel
+                      </Button>
+                    </Box>
+                  </Grid>
                 </Grid>
-                    </Grid>
-                  </Form>
-                )}
-              </Formik>
-            </DialogContent>
-          </Dialog>
+              </Form>
+            )}
+          </Formik>
+        </DialogContent>
+      </Dialog>
     </>
-  )
+  );
 }
 
-export default AddModal
+export default AddModal;
